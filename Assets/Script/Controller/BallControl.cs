d3920f7554adbed88b8d65bc1416341a35f7fd2a
M_E <cstdio@vip.qq.com>
2020-12-20 01:43:06 +0800

    Initial commit

---
+++ Assets/Script/Controller/BallControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Rigidbody m_rigidbody;
+    void Start()
+    {
+        m_rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+        float y = 0;
+        if(Input.GetButton("Jump"))
+        {
+            y = 5.0f * m_rigidbody.mass;
+        }
+        if(Mathf.Abs(x) > 1e-5f || Mathf.Abs(z) > 1e-5f || y > 1e-5f)
+        {
+            m_rigidbody.AddForce(x, y, z);
+        }
+    }
+}
---
+++ Assets/Script/Controller/CameraControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    [SerializeField]
+    GameObject m_centerObject;
+
+    Vector3 dv;
+    // Start is called before the first frame update
+    void Start()
+    {
+        dv = transform.position - m_centerObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = m_centerObject.transform.position + dv;
+    }
+}
